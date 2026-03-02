257cc85f51e2e4439e117772763235b80e077ee4
neubauera <neubauer_a@icloud.com>
2026-03-02 13:57:57 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="achim-tracker-v2";
-const ASSETS=["./","./index.html","./app.js","./manifest.webmanifest","./sw.js"];
-self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener("fetch",(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
